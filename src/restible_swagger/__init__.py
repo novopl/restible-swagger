742aee613097b331c04b9b2d5bbcd375e9195af2
Mateusz Klos <novopl@gmail.com>
2019-03-07 10:26:59 +0000

    Refactor decorators
    (feature) New `route_params` decorator for ease way to add route params to any route handler.
    (change) Refactored the decorators implementation to reuse a lot of the code and make it more maintainable.

--- src/restible_swagger/__init__.py
+++ src/restible_swagger/__init__.py
@@ -24,6 +24,7 @@ from .decorators import response_401
 from .decorators import response_403
 from .decorators import response_404
 from .decorators import responses
+from .decorators import route_params
 from .logic import extract_api_spec
 from .util import RESPONSE_401
 from .util import RESPONSE_404
